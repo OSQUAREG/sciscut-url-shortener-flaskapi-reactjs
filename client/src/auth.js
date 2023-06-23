d06ef1e922c93bd9e0263c48523bdd1c5bdc8fae
Gregory Ogbemudia <gregory.ogbemudia@gmail.com>
2023-06-23 21:07:19 +0100

    fixed Navbar and auth.js

--- client/src/auth.js
+++ client/src/auth.js
@@ -9,3 +9,31 @@ export const { useAuth, authFetch, login, logout } = createAuthProvider({
         body: token.refresh_token,
     }).then(response => response.json()),
 });
+
+
+export const logoutUser = () => {
+    let token = localStorage.getItem("REACT_TOKEN_AUTH_KEY");
+
+    const headers = new Headers({
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${JSON.parse(token)}`
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: headers,
+    };
+
+    fetch(`${baseUrl}/auth/logout`, requestOptions)
+        .then(response => {
+            // console.log(response.status)
+            if (response.status === 200 || response.status === 401) {
+                localStorage.removeItem("REACT_TOKEN_AUTH_KEY");
+                logout();
+                window.location.href = "/login";
+            } else {
+                throw new Error("Logout Failed, Please try again.");
+            }
+        })
+        .catch(error => console.log(error));
+};
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { logout, useAuth } from '../auth';
+import { logoutUser, useAuth } from '../auth';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { baseUrl, emailRegex, usernameRegex } from '..';
 import { Link } from 'react-router-dom';
-import { Button, Col, Dropdown, Form, Modal, NavItem, Row } from 'react-bootstrap';
+import { Button, Col, Dropdown, Form, InputGroup, Modal, NavItem, Row } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import User from './User';
 
@@ -16,7 +16,7 @@ const LoggedInLinks = () => {
             <Nav.Link className="nav-link" href="/">Home</Nav.Link>
             <Nav.Link className="nav-link" href="/shorten">Shorten URL</Nav.Link>
             <Nav.Link className="nav-link" href="/features">Features</Nav.Link>
-            <Nav.Link className="nav-link" href="#" onClick={() => logout()}>Log out</Nav.Link>
+            <Nav.Link className="nav-link" href="#" onClick={() => logoutUser()}>Log out</Nav.Link>
         </>
     )
 };
@@ -35,10 +35,9 @@ const LoggedOutLinks = () => {
 const NavBar = () => {
 
     const [logged] = useAuth();
-    // const { register, handleSubmit, watch, setValue, formState: { errors } } = useForm();
-    // const { setValue } = useForm();
-    const [currentUser, setCurrentUser] = useState(false);
-    const [userId, setUserId] = useState(0)
+    const { register, handleSubmit, watch, setValue, formState: { errors } } = useForm();
+    const [currentUser, setCurrentUser] = useState();
+    // const [userId, setUserId] = useState(0)
 
     let token = localStorage.getItem("REACT_TOKEN_AUTH_KEY")
     const requestOptions = {
@@ -48,7 +47,13 @@ const NavBar = () => {
 
     useEffect(() => {
         fetch(`${baseUrl}/user`, requestOptions)
-            .then(response => response.json())
+            .then(response => {
+                console.log(response.status)
+                if (response.status === 401) {
+                    logoutUser();
+                }
+                return response.json()
+            })
             .then(data => {
                 setCurrentUser(data.data)
                 console.log(currentUser)
@@ -64,9 +69,9 @@ const NavBar = () => {
     const closeUpdateModal = () => { setShowUpdate(false) };
     const showUpdateModal = (id) => {
         setShowUpdate(true);
-        setUserId(id);
-        // setValue("username", currentUser.username)
-        // setValue("email", currentUser.email)
+        // setUserId(id);
+        setValue("username", currentUser?.username)
+        setValue("email", currentUser?.email)
     };
 
     const [showChangePassword, setShowChangePassword] = useState(false);
@@ -74,49 +79,34 @@ const NavBar = () => {
     const showChangePasswordModal = () => { setShowChangePassword(true) };
 
     const UpdateUserModal = () => {
-        const { register, handleSubmit, setValue, watch, formState: { errors } } = useForm();
-        // console.log(user);
-
-        // setUserId(id);
-        setValue("username", currentUser.username)
-        setValue("email", currentUser.email)
-
-        // const [showUpdate, setShowUpdate] = useState(false);
-        // const closeUpdateModal = () => { setShowUpdate(false) };
-        // const showUpdateModal = (id) => {
-        //     setShowUpdate(true);
-        //     setUserId(id);
-        //     setValue("username", currentUser.username)
-        //     setValue("email", currentUser.email)
-        // };
+        const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+        const toggleShowConfirmPassword = () => {
+            setShowConfirmPassword(!showConfirmPassword);
+        };
 
         const updateUser = (data) => {
             console.log("user updated", data);
-            // const headers = new Headers({
-            //     "Content-Type": "application/json",
-            //     "Authorization": `Bearer ${JSON.parse(token)}`
-            // });
 
-            // const requestOptions = {
-            //     method: "PUT",
-            //     headers: headers,
-            //     body: JSON.stringify(data),
-            // }
+            const headers = new Headers({
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${JSON.parse(token)}`
+            });
 
-            // fetch(`${baseUrl}/user/`, requestOptions)
-            //     .then(response => response.json())
-            //     .then(data => {
-            //         console.log(data.data)
+            const requestOptions = {
+                method: "PUT",
+                headers: headers,
+                body: JSON.stringify(data),
+            }
 
-            //         const reload = window.location.reload()
-            //         reload()
-            //     })
-            //     .catch(error => console.log(error))
+            fetch(`${baseUrl}/user`, requestOptions)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.data)
+                    const reload = window.location.reload()
+                    reload()
+                })
+                .catch(error => console.log(error))
         };
-
-        // const changePassword = (data) => {
-        //     console.log("password changed", data)
-        // };
 
         return (
             <>
@@ -134,8 +124,8 @@ const NavBar = () => {
                     </Modal.Header>
                     <Modal.Body>
                         <User
-                            username={currentUser.username}
-                            email={currentUser.email}
+                            username={currentUser?.username}
+                            email={currentUser?.email}
                         />
                     </Modal.Body>
                     <Modal.Footer>
@@ -190,9 +180,13 @@ const NavBar = () => {
                             </Form.Group>
                             <Form.Group className="mb-3">
                                 <Form.Label className="form-label" >Confirm Password</Form.Label>
-                                <Form.Control type="password" placeholder="Confirm password"
-                                    {...register("password", { required: true })}
-                                />
+                                <InputGroup>
+                                    <Form.Control type={showConfirmPassword ? "text" : "password"} placeholder="Confirm password"
+                                        {...register("password", { required: true })}
+                                    />
+                                    <Button variant="secondary btn-sm" type="button" onClick={toggleShowConfirmPassword}
+                                    >{showConfirmPassword ? "Hide Password" : "Show Password"}</Button>
+                                </InputGroup>
                                 {errors.password && <small style={{ color: "red" }}>Password is required to save changes. </small>}
                             </Form.Group>
                         </Form>
@@ -207,13 +201,48 @@ const NavBar = () => {
     };
 
     const ChangeUserPasswordModal = () => {
-        // const { register, handleSubmit, watch, formState: { errors } } = useForm();
+        const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+        const [showPassword, setShowPassword] = useState(false);
+        const toggleShowPassword = () => {
+            setShowPassword(!showPassword);
+        };
+
+        const [showNewPassword, setShowNewPassword] = useState(false);
+        const toggleShowNewPassword = () => {
+            setShowNewPassword(!showNewPassword);
+        };
+
+        const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+        const toggleShowConfirmPassword = () => {
+            setShowConfirmPassword(!showConfirmPassword);
+        };
         const changePassword = (data) => {
             console.log("password changed", data)
+
+            const headers = new Headers({
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${JSON.parse(token)}`
+            });
+
+            const requestOptions = {
+                method: "PATCH",
+                headers: headers,
+                body: JSON.stringify(data),
+            }
+
+            fetch(`${baseUrl}/user/change-password`, requestOptions)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.data)
+                    const reload = window.location.reload()
+                    reload()
+                })
+                .catch(error => console.log(error))
         };
         return (
             <>
-                {/* <Modal
+                <Modal
                     show={showChangePassword}
                     size="lg"
                     onHide={closeChangePasswordModal}
@@ -230,42 +259,54 @@ const NavBar = () => {
                             <Form.Group as={Row}>
                                 <Form.Label column >Email</Form.Label>
                                 <Col>
-                                    <Form.Control type="email" plaintext readOnly defaultValue={`${currentUser.email}`} style={{ fontWeight: "bold" }} />
+                                    <Form.Control type="email" plaintext readOnly defaultValue={`${currentUser?.email}`} style={{ fontWeight: "bold" }} />
                                 </Col>
                             </Form.Group>
                             <br />
                             <Form.Group className="mb-3">
                                 <Form.Label className="form-label" >Old Password</Form.Label>
-                                <Form.Control type="password" placeholder="Enter your password"
-                                    {...register("old_password", {
-                                        required: { value: true, message: "Old Password is required" }
-                                    })}
-                                />
+                                <InputGroup>
+                                    <Form.Control type={showPassword ? "text" : "password"} placeholder="Enter your password"
+                                        {...register("old_password", {
+                                            required: { value: true, message: "Old Password is required" }
+                                        })}
+                                    />
+                                    <Button variant="secondary btn-sm" type="button" onClick={() => toggleShowPassword()}
+                                    >{showPassword ? "Hide Password" : "Show Password"}</Button>
+                                </InputGroup>
                                 {errors.old_password && <small style={{ color: "red" }}>{errors.old_password.message}</small>}
                             </Form.Group>
                             <br />
                             <Form.Group className="mb-3">
                                 <Form.Label className="form-label" >New Password</Form.Label>
-                                <Form.Control type="password" placeholder="Confirm your password"
-                                    {...register("new_password", {
-                                        required: { value: true, message: "New Password is required" },
-                                        minLength: { value: 8, message: "Password must be at least 8 characters." },
-                                        maxLength: { value: 25, message: "Password must not exceed 25 characters." }
-                                    })}
-                                />
+                                <InputGroup>
+                                    <Form.Control type={showNewPassword ? "text" : "password"} placeholder="Confirm your password"
+                                        {...register("new_password", {
+                                            required: { value: true, message: "New Password is required" },
+                                            minLength: { value: 8, message: "Password must be at least 8 characters." },
+                                            maxLength: { value: 25, message: "Password must not exceed 25 characters." }
+                                        })}
+                                    />
+                                    <Button variant="secondary btn-sm" type="button" onClick={() => toggleShowNewPassword()}
+                                    >{showNewPassword ? "Hide Password" : "Show Password"}</Button>
+                                </InputGroup>
                                 {errors.new_password && <small style={{ color: "red" }}>{errors.new_password.message}</small>}
                             </Form.Group>
                             <br />
                             <Form.Group className="mb-3">
-                                <Form.Label className="form-label" >Confirm Password</Form.Label>
-                                <Form.Control type="password" placeholder="Confirm your password"
-                                    {...register("confirm_password", {
-                                        required: { value: true, message: "New Password is required" },
-                                        minLength: { value: 8, message: "Password must be at least 8 characters." },
-                                        maxLength: { value: 25, message: "Password must not exceed 25 characters." },
-                                        validate: (value) => value === watch("new_password") || "Passwords do not match"
-                                    })}
-                                />
+                                <Form.Label className="form-label" >Confirm New Password</Form.Label>
+                                <InputGroup>
+                                    <Form.Control type={showConfirmPassword ? "text" : "password"} placeholder="Confirm your password"
+                                        {...register("confirm_password", {
+                                            required: { value: true, message: "New Password is required" },
+                                            minLength: { value: 8, message: "Password must be at least 8 characters." },
+                                            maxLength: { value: 25, message: "Password must not exceed 25 characters." },
+                                            validate: (value) => value === watch("new_password") || "Passwords do not match"
+                                        })}
+                                    />
+                                    <Button variant="secondary btn-sm" type="button" onClick={() => toggleShowConfirmPassword()}
+                                    >{showConfirmPassword ? "Hide Password" : "Show Password"}</Button>
+                                </InputGroup>
                                 {errors.confirm_password && <small style={{ color: "red" }}>{errors.confirm_password.message}</small>}
                             </Form.Group>
                         </Form>
@@ -274,15 +315,15 @@ const NavBar = () => {
                         <Button variant="primary" onClick={handleSubmit(changePassword)} >Save</Button>
                         <Button variant="secondary" onClick={closeChangePasswordModal}>Close</Button>
                     </Modal.Footer>
-                </Modal> */}
+                </Modal>
             </>
         )
-    }
+    };
 
     const LoggedInUser = () => {
         return (
             <>
-                Active User: <Link style={{ color: "green", fontWeight: "bold", fontSize: "25px" }} >{currentUser.username}</Link>
+                Active User: <Link style={{ color: "green", fontWeight: "bold", fontSize: "25px" }} >{currentUser?.username}</Link>
             </>
         )
     }
@@ -302,13 +343,9 @@ const NavBar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-
                         {logged ? <LoggedInLinks /> : <LoggedOutLinks />}
-
                     </Nav>
                 </Navbar.Collapse>
-            </Container>
-            <Container>
                 <Navbar.Collapse className="justify-content-end">
                     <Navbar.Text>
                         {logged ?
