1827a41cead304c27cc45209df000ae5aa238797
Gregory Ogbemudia <gregory.ogbemudia@gmail.com>
2023-07-01 22:23:44 +0100

    QR Code display fix

--- client/src/components/Url.js
+++ client/src/components/Url.js
@@ -84,7 +84,7 @@ export const URLDetails = ({ id, title, long_url, short_url, date_created, visit
                         {/* <p>{qr_code_id}</p> */}
                         {/* <img src={window.location.origin + imagePath1} alt="QR Code" rounded fluid />
                         <br /> */}
-                        <img src={`${process.env.PUBLIC_URL}/${imagePath}`} alt="QR Code" rounded fluid />
+                        <img src={process.env.PUBLIC_URL + imagePath} alt="QR Code" rounded fluid />
                         <Button className="btn btn-sm m-2" variant="secondary" onClick={() => downloadImage(imagePath, imageName)}>
                             <FontAwesomeIcon icon={faDownload} />{" "}Download QR Code
                         </Button>
